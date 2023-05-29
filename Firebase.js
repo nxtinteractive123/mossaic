c69dd7cdfe05ed88515056c7cd40b55c72f1a6eb
nxtinteractive123 <133853464+nxtinteractive123@users.noreply.github.com>
2023-05-29 09:27:04 +0530

    new firebase information added
    Please replace the current firebase.js file with this firebase.js file so that it can place the data in our firebase  
    you can find the firebase.js file in the src folder

---
+++ Firebase.js
@@ -0,0 +1,27 @@
+// import { initializeApp } from "firebase/app";
+// import 'firebase/storage';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+import { getStorage } from "firebase/storage";
+
+
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAL5YpfpgBX7VGGfuVLTA9VhYNZiPuLMRE",
+  authDomain: "mosaic-a3974.firebaseapp.com",
+  projectId: "mosaic-a3974",
+  storageBucket: "mosaic-a3974.appspot.com",
+  messagingSenderId: "926508018637",
+  appId: "1:926508018637:web:304fd97774bb2078877284",
+  measurementId: "G-7GLQ09QGW1"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const storage = getStorage(app);
