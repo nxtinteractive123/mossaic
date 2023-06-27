3bbe821589fcd2413f6ae29610770884bd3495c7
nxtinteractive123 <133853464+nxtinteractive123@users.noreply.github.com>
2023-06-28 04:12:45 +0530

    updated latest code with bug fixed
    added firebase auth anonymous
    
    updated app.js, firebase.js, cameranew.js

--- mosaic_latest/src/Firebase.js
+++ mosaic_latest/src/Firebase.js
@@ -2,23 +2,30 @@
 // import 'firebase/storage';
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAL5YpfpgBX7VGGfuVLTA9VhYNZiPuLMRE",
-  authDomain: "mosaic-a3974.firebaseapp.com",
-  projectId: "mosaic-a3974",
-  storageBucket: "mosaic-a3974.appspot.com",
-  messagingSenderId: "926508018637",
-  appId: "1:926508018637:web:304fd97774bb2078877284",
-  measurementId: "G-7GLQ09QGW1"
+  apiKey: "AIzaSyCm_cLKEPTa1zv8p7IrAmJ4jbZsJYDA8oA",
+  authDomain: "mosaic1-d1227.firebaseapp.com",
+  projectId: "mosaic1-d1227",
+  storageBucket: "mosaic1-d1227.appspot.com",
+  messagingSenderId: "153682397608",
+  appId: "1:153682397608:web:4b8321dde8ea84da0cb59d",
+  measurementId: "G-N3DJKE4LFR"
 };
-
-
-
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+
+const auth = getAuth();
+signInAnonymously(auth)
+  .then(() => {
+    // Signed in..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+export const db = getFirestore(app);
 export const storage = getStorage(app);
